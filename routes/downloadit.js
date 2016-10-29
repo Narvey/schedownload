5d9aded0435183338619a7d09bf1ca18a0c33417
Narvey <nthnhrvy@gmail.com>
2016-10-28 23:46:19 -0600

    should at least store the requests now

--- routes/downloadit.js
+++ routes/downloadit.js
@@ -1,8 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
+express.use('/files',express.static('/home/chip/bin/NodeServer/files'));//have static folder available
+
 /* respond to GET api for downloading. */
-router.get('/', function(req, res, next) {
+router.get('/*', function(req, res, next) {
+  fs.open('./downloads.sh','a+',,function(err,fd) {
+    if (err) return console.error(err);
+    fs.write(fd, 'echo \"' + req.URL + '\" >> /home/chip/bin/NodeServer/files/list.txt');//TODO: sanitize input and actually wget.
+  });
   res.send('Download scheduled! <br>'); //You should see your file at 192.168.0.115/files/'+'');
 });
 
