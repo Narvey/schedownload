b1892e268581bb23ab528ebe16f6fefe284ee2ab
Narvey <nthnhrvy@gmail.com>
2016-10-31 07:56:30 -0600

    split string
    also need to actually wget, as well as clear download.sh each time.

--- routes/downloadit.js
+++ routes/downloadit.js
@@ -4,10 +4,10 @@ var fs = require('fs');
 
 /* respond to GET api for downloading. */
 router.get('/', function(req, res, next) {
-  debugger;
   fs.open('./downloads.sh','a+',function(err,fd) {
-    if (err) return console.error(err); debugger;
-    fs.write(fd, 'echo \"' + req.url + '\" >> /home/chip/bin/NodeServer/files/list.txt;');//TODO: sanitize input and actually wget.
+    if (err) return console.error(err);
+    var params = req.url.split('?');
+    fs.write(fd, 'echo \"' + params + '\" >> /home/chip/bin/NodeServer/files/list.txt;');//TODO: sanitize input and actually wget.
   });
   res.send('Download scheduled! <br>'); //You should see your file at 192.168.0.115/files/'+'');
 });
