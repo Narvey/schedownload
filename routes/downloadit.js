21a0cdde9a1cb8f48b624dd2d5f2af9c272570ff
Nathan Harvey <nthnhrvy@gmail.com>
2016-10-31 17:38:01 -0600

    Parse out the question mark This should finally have a clean URL in the output file. and hopefully it also predicts the filename wget will use as well.

--- routes/downloadit.js
+++ routes/downloadit.js
@@ -2,14 +2,20 @@ var express = require('express');
 var router = express.Router();
 var fs = require('fs');
 
+if (!Array.prototype.last) {
+  Array.prototype.last = function() {
+    return this[this.length - 1];
+  };
+};
+
 /* respond to GET api for downloading. */
 router.get('/', function(req, res, next) {
   fs.open('./downloads.sh','a+',function(err,fd) {
     if (err) return console.error(err);
-    var params = req.url.split('?');
-    fs.write(fd, 'echo \"' + params[1] + '\" >> /home/chip/bin/NodeServer/files/list.txt;');//TODO: sanitize input and actually wget.
+    var params = req.url.replace('/?','');
+    fs.write(fd, 'echo \"' + params + '\" >> /home/chip/bin/NodeServer/files/list.txt;');//TODO: sanitize input and actually wget.
   });
-  res.send('Download scheduled! <br>'); //You should see your file at 192.168.0.115/files/'+'');
+  res.send('Download scheduled! <br>You should see your file at 192.168.0.115/files/'+params.split('/').last()+' tomorrow (and also on the thumbdrive'+'');
 });
 
 module.exports = router;
