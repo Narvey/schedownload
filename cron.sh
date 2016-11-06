11ff796b7783b8030142207d74ef217609634807
Narvey <nthnhrvy@gmail.com>
2016-11-06 07:22:34 -0700

    protecting downloads and redirecting sderr to file

--- cron.sh
+++ cron.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 sdpath="/home/chip/bin/NodeServer"; #path where you were when you cloned the sd repo
-bash $sdpath/schedownload/downloads.sh;
+exec 2>> $sdpath/logs/cron.sh.log; #redirect standard error to the log file.
+if $sdpath/schedownload/downloads.sh; then
+    echo "">$sdpath/schedownload/downloads.sh;
+fi
 
 if lsblk|grep sda; then #if the thumbdrive is present
     mount /dev/sda /media/USB1/;
@@ -8,6 +11,7 @@ if lsblk|grep sda; then #if the thumbdrive is present
     #uncomment the following line if you do NOT want files accessible from the server, but only from the thumbdrive.
     #rm /home/chip/bin/NodeServer/files/*;
     umount /dev/sda;
-else echo "downloads not copied at $(date)">>$sdpath/logs/cron.sh.log;
+    echo "1">$sdpath/maydelete;
+else echo "downloads not copied at $(date)">>$sdpath/logs/cron.sh.log;echo "0">$sdpath/maydelete;
 fi
-echo "">$sdpath/schedownload/downloads.sh
+
