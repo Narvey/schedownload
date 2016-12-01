548e3c4bfa3cb3704a0e9dc033184337388588f5
Nathan Harvey <nkarvey@outlook.com>
2016-11-30 20:48:06 -0700

    adding mount indicator LED

--- cron.sh
+++ cron.sh
@@ -7,14 +7,18 @@ fi
 
 if lsblk|grep sda1; then #if the thumbdrive is present
     mount /dev/sda1 /media/USB1/;
+    /usr/sbin/i2cset -f -y 0 0x34 0x93 0x1;
 elif lsblk|grep sda; then
     mount /dev/sda /media/USB1/;
+    /usr/sbin/i2cset -f -y 0 0x34 0x93 0x1;
+else
+    echo "$(date): thumbdrive not present">>$sdpath/logs/cron.sh.log
 fi
-if cp $sdpath/files/* /media/USB1/;
+if cp $sdpath/files/* /media/USB1/ &&
         #uncomment the following line if you do NOT want files accessible from the server, but only from the thumbdrive.
-        #rm /home/chip/bin/NodeServer/files/*;
+        #rm /home/chip/bin/NodeServer/files/* &&
         umount /dev/sda1;then
-    echo "$(date): Downloads copied to external drive">>$sdpath/logs/cron.sh.log;
-    else echo "$(date): thumbdrive not present">>$sdpath/logs/cron.sh.log
+    echo "$(date): Downloads copied to drive">>$sdpath/logs/cron.sh.log;
+    else echo "$(date): error copying to thumbdrive">>$sdpath/logs/cron.sh.log
 fi
-
+/usr/sbin/i2cset -f -y 0 0x34 0x93 0x0
